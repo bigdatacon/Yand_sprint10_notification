92131ea0ecb8cf112908983f2722d9a4b7681e1c
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-08-23 11:44:55 +0300

    только мое

--- email/enrich_my/main.py
+++ email/enrich_my/main.py
@@ -30,6 +30,8 @@ async def return_user_info_by_id(db_name):
     user_info = AuthServiceMy(connection).get_all_users_info_from_table()
     return {'user_info' : user_info}
 
+
+
 # answer = requests.get("http://127.0.0.1:8000/user/", params={'db_name' : 'auth', 'user_id' : 'a61846cf-8882-4213-a471-f763000d1147'})
 # print(f' eto answer : {answer}')
 # @app.post("/package/{priority}")
